3aa656a1ded0ede57cf49e6f5104b5e5ddc80de0
Thatpo <thatos1234@gmail.com>
2021-07-10 01:13:58 -0400

    putting up starting file

---
+++ script.js
@@ -0,0 +1,15 @@
+function looping() {
+    for (let i = 1; i <= 7; i++) {
+        console.log("#\n");
+    }
+
+}
+
+function cool() {
+
+    console.log("hello");
+    debugger;
+    console.log("cry");
+}
+
+cool();
